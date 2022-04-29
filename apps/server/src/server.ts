92e52cd7d93595b07584ffaa1995aa5a2ceaf47e
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-04-29 19:42:56 +0200

    Don't touch anything, it's working 🎉

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -4,20 +4,10 @@ import app from './app/app'
 
 const fastify = app()
 
-const port = process.env.PORT || 3000
-
-// server.listen(port, (err, address) => {
-//   if (err) {
-//     console.error(err)
-//     process.exit(1)
-//   }
-//   console.log(`Server listening at ${address}`)
-// })
-
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(port, '0.0.0.0')
+    await fastify.listen(8080, '0.0.0.0')
     // @ts-ignore
     fastify.log.info(`Server listening on ${fastify.server.address().port}`)
   } catch (err) {
