debca6bbfcf4aa240086e6854632d5b1674d39b9
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-05-03 00:23:26 +0200

    Oups, also 0.0.0.0 needed for heroku

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -6,7 +6,7 @@ const server = app()
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, (err, address) => {
+server.listen(PORT, '0.0.0.0', (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
