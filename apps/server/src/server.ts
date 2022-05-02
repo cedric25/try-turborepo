f97e23687fae2ff1247a7217b72d54947a89b9c7
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-05-02 23:46:34 +0200

    Switch back to 'normal' fastify server

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -2,18 +2,14 @@ import 'module-alias/register'
 
 import app from './app/app'
 
-const fastify = app()
+const server = app()
 
-// Run the server!
-const start = async () => {
-  try {
-    await fastify.listen(8080, '0.0.0.0')
-    // @ts-ignore
-    fastify.log.info(`Server listening on ${fastify.server.address().port}`)
-  } catch (err) {
-    fastify.log.error(err)
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, (err, address) => {
+  if (err) {
+    console.error(err)
     process.exit(1)
   }
-}
-
-start()
+  console.log(`Server listening at ${address}`)
+})
