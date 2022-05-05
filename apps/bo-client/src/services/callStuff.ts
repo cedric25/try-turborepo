0c0ec43dfe45f88c30b0d1b76024450624b7011f
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2022-05-05 15:32:06 +0200

    prettier

--- apps/bo-client/src/services/callStuff.ts
+++ apps/bo-client/src/services/callStuff.ts
@@ -1,21 +1,21 @@
-import { createTRPCClient } from "@trpc/client";
-import type { AppRouter } from "../../../bo-server/src/router/index";
+import { createTRPCClient } from '@trpc/client'
+import type { AppRouter } from '../../../bo-server/src/router/index'
 
 const client = createTRPCClient<AppRouter>({
-  url: "http://localhost:3011/trpc",
-});
+  url: 'http://localhost:3011/trpc',
+})
 
 export async function callStuff() {
-  const helloResponse = await client.query("hello", {
-    username: "Plof",
-  });
+  const helloResponse = await client.query('hello', {
+    username: 'Plof',
+  })
 
-  console.log("helloResponse", helloResponse);
+  console.log('helloResponse', helloResponse)
 }
 
 export function goCreateFruit() {
-  return client.mutation("create", {
-    name: "Banana",
-    color: "Yellow",
-  });
+  return client.mutation('create', {
+    name: 'Banana',
+    color: 'Yellow',
+  })
 }
